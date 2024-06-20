21e60b5ce28ca03ed2e9015e3cc99002e24ba46e
Melow04 <corteskal04@gmail.com>
2024-06-20 18:39:33 +0800

    Hello

--- F6/assets/regvalidation.js
+++ F6/assets/regvalidation.js
@@ -4,3 +4,37 @@ function submitToPage(){
 }
 
 
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let confpassword = document.getElementById("confpassword");
+let bdayInput = document.getElementById("birthday");
+let submit = document.getElementById("submit");
+
+bdayInput.addEventListener('change',function(){
+    let bday = document.getElementById("birthday").value;
+    let date = new Date();
+    let age = date.getFullYear() - bday;
+
+    document.getElementById("age").value = age;
+});
+
+email.addEventListener("keypress",function(){
+    if(email.value != ""){
+        email.setAttribute("class","form-control");
+    }
+});
+
+submit.addEventListener('click', function(){
+    // Check if email is empty
+    if(email.value == ""){
+        email.setAttribute("class","form-control empty");
+    }
+});
+
+submit.addEventListener('mouseover', function(){
+    submit.setAttribute("class","btn btn-primary button-mouse-over");
+});
+
+submit.addEventListener('mouseout', function(){
+    submit.setAttribute("class","btn btn-primary");
+});
