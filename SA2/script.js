f3137317e43aae3547bcd7ef447acedfb8fc2b44
Melow04 <corteskal04@gmail.com>
2025-02-11 09:38:51 +0800

    final

--- SA2/script.js
+++ SA2/script.js
@@ -1,15 +1,18 @@
-
-function changeColor() {
-    var color = document.getElementById("color").value;
-    document.body.style.borderColor = color;
-    
-
-
-
-
-
-
-
-
-
+function changeColor(day) {
+    const colors = {
+        Monday: '#cf1717',
+        Tuesday: '#17cf17',
+        Wednesday: '#1717cf',
+        Thursday: '#cfcf17',
+        Friday: '#cf17cf',
+        Saturday: '#17cfcf',
+        Sunday: '#6339c7'
+    };
+    const box = document.querySelector('.box');
+    box.style.transform = 'translateY(-3000px)';
+    setTimeout(() => {
+        box.style.transition = 'transform 0.5s ease-in, border-color 0.5s ease-in';
+        box.style.transform = 'translateY(0px)';
+        box.style.borderColor = colors[day];
+    }, 300);
 }
